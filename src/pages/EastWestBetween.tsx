7e2935892ece4f48bc0fdd1406d6fbf7328e5b96
Zach Werbalowsky <zach@c3backflow.com>
2025-09-04 17:12:04 -0700

    no message

--- src/pages/EastWestBetween.tsx
+++ src/pages/EastWestBetween.tsx
@@ -1,8 +1,13 @@
 import ImageGallery from '@/components/ImageGallery';
+import spread1 from '@/assets/Spread-1.jpg';
 
 const EastWestBetween = () => {
   const images = [
-
+    {
+      src: spread1,
+      alt: 'Contemplative portrait study',
+      title: 'Between Worlds, 2024'
+    }
   ];
 
   return <ImageGallery images={images} title="East, West and In Between" />;
