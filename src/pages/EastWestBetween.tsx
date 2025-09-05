370f08f23658fcaba666230cc7b8d574e380d73b
Zach Werbalowsky <zach@c3backflow.com>
2025-09-04 17:08:43 -0700

    removed images from east west

--- src/pages/EastWestBetween.tsx
+++ src/pages/EastWestBetween.tsx
@@ -1,13 +1,8 @@
 import ImageGallery from '@/components/ImageGallery';
-import spread1 from '@/assets/spread-1.jpg';
 
 const EastWestBetween = () => {
   const images = [
-    {
-      src: spread1,
-      alt: 'Contemplative portrait study',
-      title: 'Between Worlds, 2024'
-    }
+
   ];
 
   return <ImageGallery images={images} title="East, West and In Between" />;
